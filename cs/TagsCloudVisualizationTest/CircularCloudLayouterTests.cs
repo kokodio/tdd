f6d7978a1360ffa505ee50fd6f6d082502c91a52
kokodio <kirill.zaripow@gmail.com>
2024-11-11 01:18:48 +0500

    refactor CircularCloudLayouterTests

--- cs/TagsCloudVisualizationTest/CircularCloudLayouterTests.cs
+++ cs/TagsCloudVisualizationTest/CircularCloudLayouterTests.cs
@@ -9,140 +9,227 @@ public class CircularCloudLayouterTests
 {
     private CircularCloudLayouter layout;
 
+    private List<Rectangle> rectangles;
+
     [SetUp]
     public void SetUp()
     {
         layout = new CircularCloudLayouter();
+        rectangles = [];
     }
-    
+
     [Test]
     public void PutNextRectangle_ReturnRectangleEmpty_OnSizeEmpty()
     {
         layout.PutNextRectangle(Size.Empty).Should().BeEquivalentTo(Rectangle.Empty);
     }
-    
-    [TestCase(0,0)]
-    [TestCase(5,5)]
-    [TestCase(0,5)]
+
+    [TestCase(0, 0)]
+    [TestCase(5, 5)]
+    [TestCase(0, 5)]
     public void PutNextRectangle_ReturnLocationZero_OnFirstInput(int height, int width)
     {
-        var layout = new CircularCloudLayouter();
         var size = new Size(width, height);
-        var expected = new Rectangle(0,0,width,height);
-        
-        layout.PutNextRectangle(size).Should().BeEquivalentTo(expected);
-    }
-    
-    [TestCase(0,0)]
-    [TestCase(5,5)]
-    [TestCase(0,5)]
-    public void PutNextRectangle_ReturnLocationZero_AfterResetLayout(int height, int width)
-    {
-        var layout = new CircularCloudLayouter();
-        var size = new Size(width, height);
-        var expected = new Rectangle(0,0,width,height);
+        var expected = new Rectangle(0, 0, width, height);
+        var result = layout.PutNextRectangle(size);
 
-        for (var i = 0; i < 10; i++)
-        {
-            layout.PutNextRectangle(size);
-        }
-        
-        layout.ResetLayout();
-        
-        layout.PutNextRectangle(size).Should().BeEquivalentTo(expected);
+        rectangles.Add(result);
+
+        result.Should().BeEquivalentTo(expected);
     }
-    
-    [TestCase(-1,0)]
-    [TestCase(-1,5)]
-    [TestCase(5,-1)]
-    [TestCase(0,-1)]
+
+    [TestCase(-1, 0)]
+    [TestCase(-1, 5)]
+    [TestCase(5, -1)]
+    [TestCase(0, -1)]
     public void PutNextRectangle_ThrowArgumentException_WhenSizeIsNegative(int height, int width)
     {
         var size = new Size(width, height);
-        
-        
+
         var act = () => layout.PutNextRectangle(size);
         
-        act.Should().Throw<ArgumentException>("Размер меньше 0");
+        act.Should().Throw<ArgumentOutOfRangeException>($"{size} меньше 0");
     }
 
     [Test]
-    public void GetAllRectangles_BeEmpty_OnInitialize()
+    public void IntersectsWith_BeFalse()
     {
-        layout.GetAllRectangles().Should().BeEmpty();
-    }
-    
-    [Test]
-    public void GetAllRectangles_BeEmpty_AfterResetLayout()
-    {
-        layout.PutNextRectangle(Size.Empty);
-        layout.GetAllRectangles().Should().HaveCount(1);
-        
-        layout.ResetLayout();
-        layout.GetAllRectangles().Should().BeEmpty();
-    }
-    
-    [Test]
-    public void GetAllRectangles_NotBeEmpty_WhenValidPutNextRectangle()
-    {
-        layout.PutNextRectangle(Size.Empty);
-        
-        layout.GetAllRectangles().Should().NotBeEmpty();
-    }
-    
-    [TestCase(0)]
-    [TestCase(5)]
-    [TestCase(10)]
-    public void GetAllRectangles_CountEqualPutNextRectangleInvokes_WhenValidPutNextRectangle(int count)
-    {
-        var layout = new CircularCloudLayouter();
+        layout = LayoutRegistry.DefaultLayout();
+        rectangles = LayoutRegistry.DefaultLayoutRectangles;
 
-        for (var i = 0; i < count; i++)
-        {
-            layout.PutNextRectangle(SizeRegistry.DefaultSize);
-        }
-
-        layout.GetAllRectangles().Should().HaveCount(count);
-    }
-    
-    [TestCase(0,0)]
-    [TestCase(0,10)]
-    [TestCase(10,50)]
-    public void IntersectsWith_BeFalse_OnRandomData(int sizeFrom, int sizeTo)
-    {
-        var random = new Random();
-        
-        for (var i = 0; i < 1000; i++)
-        {
-            var randomSize = new Size(random.Next(sizeFrom, sizeTo), random.Next(sizeFrom, sizeTo));
-            layout.PutNextRectangle(randomSize);
-        }
-        
-        var rect = layout.GetAllRectangles();
-        
-        var isIntersect = false;
-        for (var i = 0; i < rect.Length; i++)
-        {
-            for (var j = i + 1; j < rect.Length; j++)
-            {
-                if (!rect[i].IntersectsWith(rect[j])) continue;
-                isIntersect = true;
-                break;
-            }
-            if (isIntersect) break;
-        }
+        var isIntersect = rectangles
+            .SelectMany((rect, i) => rectangles.Skip(i + 1).Select(r => (rect, r)))
+            .Any(pair => pair.rect.IntersectsWith(pair.r));
 
         isIntersect.Should().BeFalse();
     }
+
+    [Test]
+    [Repeat(5)]
+    public void Layout_BeCircle_WhenDeviationIsLessThanTolerance()
+    {
+        layout = LayoutRegistry.DefaultLayout();
+        rectangles = LayoutRegistry.DefaultLayoutRectangles;
+
+        var totalArea = 0d;
+        var weightedXSum = 0d;
+        var weightedYSum = 0d;
+        var maxRadius = 0d;
+
+        foreach (var rect in rectangles)
+        {
+            maxRadius = Math.Max(maxRadius, GetDistanceToCenter(rect));
+            
+            var centerX = rect.X + rect.Width / 2.0;
+            var centerY = rect.Y + rect.Height / 2.0;
+            var area = rect.Width * rect.Height;
+            
+            weightedXSum += centerX * area;
+            weightedYSum += centerY * area;
+            
+            totalArea += area;
+        }
+
+        var centerXOfMass = weightedXSum / totalArea;
+        var centerYOfMass = weightedYSum / totalArea;
+        var deviationLenght = Math.Sqrt(Math.Pow(centerXOfMass, 2) + Math.Pow(centerYOfMass, 2));
+
+        var toleranceValue = maxRadius * 0.15;
+
+        deviationLenght.Should().BeLessThan(toleranceValue);
+    }
     
+    [Test]
+    [Repeat(5)]
+    public void Layout_BeDense()
+    {
+        layout = LayoutRegistry.DefaultLayout();
+        rectangles = LayoutRegistry.DefaultLayoutRectangles;
+        
+        var outliersCount = (int)(rectangles.Count * 0.02);
+        
+        var totalArea = rectangles
+            .Select(x => x.Height * x.Width)
+            .Sum();
+        
+        var maxRadius = rectangles
+            .Select(GetDistanceToCenter)
+            .OrderDescending()
+            .Skip(outliersCount)
+            .First();
+
+        var circleArea = Math.PI * Math.Pow(maxRadius, 2);
+        
+        var dense = totalArea / circleArea;
+
+        dense.Should().BeGreaterThan(0.5);
+    }
+
+    [Test]
+    [Repeat(5)]
+    public void Layout_HasUniformDensityInFourDirections()
+    {
+        layout = LayoutRegistry.DefaultLayout();
+        rectangles = LayoutRegistry.DefaultLayoutRectangles;
+        
+        const int centerX = 0;
+        const int centerY = 0;
+
+        var quadrants = new Dictionary<string, int>
+        {
+            { "TopRight", 0 },
+            { "TopLeft", 0 },
+            { "BottomRight", 0 },
+            { "BottomLeft", 0 }
+        };
+        
+        foreach (var rect in rectangles)
+        {
+            var rectCenterX = rect.X + rect.Width / 2.0;
+            var rectCenterY = rect.Y + rect.Height / 2.0;
+            var area = rect.Width * rect.Height;
+
+            switch (rectCenterX)
+            {
+                case >= centerX when rectCenterY >= centerY:
+                    quadrants["TopRight"] += area;
+                    break;
+                case < centerX when rectCenterY >= centerY:
+                    quadrants["TopLeft"] += area;
+                    break;
+                case >= centerX when rectCenterY < centerY:
+                    quadrants["BottomRight"] += area;
+                    break;
+                default:
+                    quadrants["BottomLeft"] += area;
+                    break;
+            }
+        }
+
+        var totalArea = quadrants.Values.Sum();
+        
+        foreach (var quadrant in quadrants)
+        {
+            var percent = quadrant.Value / (double)totalArea;
+
+            percent.Should().BeGreaterThan(0.15, $"Плотность {quadrant.Key} ниже среднего");
+            percent.Should().BeLessThan(0.35, $"Плотность {quadrant.Key} выше среднего");
+        }
+    }
+
+    [Test]
+    [Repeat(5)]
+    public void Outliers_NotBeFarAway()
+    {
+        layout = LayoutRegistry.DefaultLayout();
+        rectangles = LayoutRegistry.DefaultLayoutRectangles;
+        
+        var outliersCount = (int)(rectangles.Count * 0.02);
+        var tenPercent = (int)(rectangles.Count * 0.1);
+
+        var sortedDistance = rectangles
+            .Select(GetDistanceToCenter)
+            .OrderDescending()
+            .ToArray();
+
+        var outliersDistance = sortedDistance
+            .Take(outliersCount)
+            .Average();
+        
+        var tenPercentDistance = sortedDistance
+            .Take(tenPercent)
+            .Average();
+        
+        var averageDistance = sortedDistance
+            .Skip(outliersCount)
+            .Average();
+        
+        var averageCoefficient = outliersDistance / averageDistance;
+        var tenPercentCoefficient = outliersDistance / tenPercentDistance;
+
+        averageCoefficient.Should().BeLessThan(1.9);
+        tenPercentCoefficient.Should().BeLessThan(1.2);
+    }
+
+    private static double GetDistanceToCenter(Rectangle x)
+    {
+        return Math.Sqrt(Math.Pow(x.X + x.Width / 2.0, 2) + Math.Pow(x.Y + x.Height / 2.0, 2));
+    }
+
     [TearDown]
     public void TearDown()
     {
-        if (TestContext.CurrentContext.Result.FailCount > 0)
+        if (TestContext.CurrentContext.Result.FailCount <= 0) return;
+
+        var render = new ContentFittingRenderer();
+        var name = $"{TestContext.CurrentContext.Test.Name}.png";
+
+        foreach (var rectangle in rectangles)
         {
-            var render = new AutoAdjustRenderer();
-            render.AddRectangles(layout.GetAllRectangles());
-            render.SaveImage("Failed.png");
+            render.AddRectangle(rectangle);
         }
+
+        render.SaveImage(name);
+        Console.WriteLine($"Tag cloud visualization saved to file {name}");
     }
 }
