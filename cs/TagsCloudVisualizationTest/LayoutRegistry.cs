768ec0df76dbe5f07d81ab859eb62775cc94113e
kokodio <kirill.zaripow@gmail.com>
2024-11-11 01:17:34 +0500

    change LayoutRegistry default

--- cs/TagsCloudVisualizationTest/LayoutRegistry.cs
+++ cs/TagsCloudVisualizationTest/LayoutRegistry.cs
@@ -5,13 +5,19 @@ namespace TagsCloudVisualizationTest;
 
 public static class LayoutRegistry
 {
-    public static CircularCloudLayouter DefaultLayout()
+    private const int DefaultLayoutRectangleCount = 1000;
+
+    public static List<Rectangle> DefaultLayoutRectangles = [];
+    
+    private static readonly Random Random = new();
+    public static CircularCloudLayouter DefaultLayout(int sizeFrom = 10, int sizeTo = 100)
     {
         var layouter = new CircularCloudLayouter();
+        DefaultLayoutRectangles = new List<Rectangle>(DefaultLayoutRectangleCount);
         
-        for (var i = 0; i < 13; i++)
+        for (var i = 0; i < DefaultLayoutRectangleCount; i++)
         {
-            layouter.PutNextRectangle(SizeRegistry.DefaultSize);
+            DefaultLayoutRectangles.Add(layouter.PutNextRectangle( new Size(Random.Next(sizeFrom, sizeTo), Random.Next(sizeFrom, sizeTo))));
         }
         
         return layouter;
